ace0154e00b2ecbc2c9c9ae85b691e9647f6c72e
Terence-lr <trichardson0826@gmail.com>
2024-12-01 22:13:55 -0500

    update

--- scripts/script.js
+++ scripts/script.js
@@ -4,15 +4,18 @@ const catContainer = document.getElementById('catContainer')
 // Function to fetch a random cat image
 async function fetchCat() {
   try {
-    console.log('Fetching cat image...') // Check if the function is called
+    console.log('Fetching cat image...')
     const response = await fetch('https://api.thecatapi.com/v1/images/search')
-    console.log('Response received:', response) // Log the response object
     const data = await response.json()
-    console.log('Data fetched:', data) // Log the fetched data
+    console.log('Data fetched:', data)
+
+    const catImage = document.createElement('img')
+    catImage.src = data[0].url
+    catImage.alt = 'Random Cat'
+    document.getElementById('catContainer').innerHTML = ''
+    document.getElementById('catContainer').appendChild(catImage)
   } catch (error) {
     console.error('Error fetching cat image:', error)
-    catContainer.innerHTML =
-      '<p>Something went wrong. Please try again later!</p>'
   }
 }
 
